ab96ae523c09d6477c3c28153977d2e726b641d8
Luiscv1719 <141294557+LuisECV0@users.noreply.github.com>
2025-01-03 21:57:26 -0500

    se añadieron los estilos de footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon4 from '../assets/img/nav-icon4.svg';
-
+import './Footer.css';
 
 export const Footer = () => {
   return (

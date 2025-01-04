5a7878cc475e4c112cead16b4f66ed7b78bfb006
Luiscv1719 <141294557+LuisECV0@users.noreply.github.com>
2025-01-03 21:34:53 -0500

    se instalo react icons

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React, { Suspense, lazy } from 'react';
 import './App.css'
 import { NavBar } from './Components/NavBar';
 import { Footer } from './Components/Footer';
+import { AboutMe } from './Components/AboutMe';
 
 function App() {
  
   return (
     <div className='App'>
       <NavBar />
+      <AboutMe />
+
       <Footer />
     </div>
   )
---
+++ src/Components/AboutMe.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Container, Row, Col } from "react-bootstrap";
+import mob from '../assets/img/mob2.svg';
+import cv from '../assets/font/Luis-Castañeda-Vega-.pdf';
+
+export const AboutMe = () => {
+  const downloadCV = () => {
+    window.open(cv, '_blank');
+  };
+
+  return (
+    <section id="about-me" className="about-me">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6}>
+            <img src={mob} alt="Perfil" className="img-fluid rounded-circle" loading="lazy" />
+          </Col>
+          <Col xs={12} md={6}>
+            <div className="about-me-text">
+              <h2>I`m LuisCV</h2>
+              <p>
+                I`m a frontend developer.
+              </p>
+              <div className="icon-dw">
+                <button onClick={downloadCV} className="btn-download-cv">Download CV</button>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
